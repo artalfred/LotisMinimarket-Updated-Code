61b7e4ce4c76aac2b6a8e860d90b14ac7b1e0aa3
art alfred <124093565+artalfred@users.noreply.github.com>
2024-07-09 06:10:00 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -23,14 +23,21 @@
 	.footer_nav ul li {
 		font-size: 16px;
 	}
+
+	.address {
+		width: 65%;
+	}
+
+	@media (max-width: 767px) {
+		.address {
+		width: 100%;
+	}
+	}
 </style>
  
 <!-- footer -->
 	<div class="footer">
 		<div class="container"> 
-			 
-		 
-		 
 			<div class="agile_footer_grids">
 
 			<div class="footer_nav">
@@ -49,6 +56,16 @@
 					<li>About</li>
 					<li><a href="tel:+09452758364" style="color: #fff;">Contact</a></li>
 				</ul>
+			</div>
+
+			<div style="margin-top: 3rem; display: flex; gap: 1rem">
+				<i class="fa fa-location-arrow" style="font-size:24px; color: #fff"></i>
+				<p style="color: #fff">ADDRESS: Private Road 200, Mandaluyong City.</p>
+			</div>
+
+			<div class="address" style="margin-top: 1rem;">
+				<p style="color: #fff">Lotis Minimarket offers a convenient online platform for customers to browse and purchase a wide range of products, which they can then pick up at their convenience from our physical store.</p>
+
 			</div>
 				 
 				
