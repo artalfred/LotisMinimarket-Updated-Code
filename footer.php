bd2e2cf1cdcee5579a3e47569df27d7c3c7653ac
art alfred <124093565+artalfred@users.noreply.github.com>
2024-07-09 00:23:19 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,61 @@
+
+<style>
+
+	
+	footer {
+		background-color: #84C639 !important;
+	}
+
+
+
+	.footer_nav {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.footer_nav ul {
+		display: flex;
+		gap: 2rem;
+		list-style: none;
+		color: #fff;
+	}
+
+	.footer_nav ul li {
+		font-size: 16px;
+	}
+</style>
+ 
+<!-- footer -->
+	<div class="footer">
+		<div class="container"> 
+			 
+		 
+		 
+			<div class="agile_footer_grids">
+
+			<div class="footer_nav">
+				<ul>
+					<li>Home</li>
+					<li>Shop</li>
+				</ul>
+
+				<ul>
+					<li>
+						<img src="./images/LONTIS.png" alt="Lotis Minimarket" style="max-height: 85px;">
+					</li>
+				</ul>
+
+				<ul>
+					<li>About</li>
+					<li><a href="tel:+09452758364" style="color: #fff;">Contact</a></li>
+				</ul>
+			</div>
+				 
+				
+			</div>
+			<div class="wthree_footer_copy">
+				<p>©2024 Lotis Minimarket. </p>
+			</div>
+		</div>
+	</div>
+<!-- //footer -->
